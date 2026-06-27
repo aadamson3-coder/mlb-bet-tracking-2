f8cc26d9fd8c0677506877d8f62fdad226307bc8
aadamson3-coder <aadamson3@gmail.com>
2026-06-27 13:10:09 -0400

    Add configuration for time zone and book priorities

---
+++ config.py
@@ -0,0 +1,13 @@
+# config.py
+
+from zoneinfo import ZoneInfo
+
+ET = ZoneInfo("America/New_York")
+
+BOOK_PRIORITY = [
+    "draftkings",
+    "betmgm",
+    "fanatics",
+    "kalshi",
+    "polymarketus",
+]
